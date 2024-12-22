c130973011254e0e7eb7baf23c4852d81b3c9507
rohanshad <rohan.shad@gmail.com>
2024-12-22 12:02:40 -0500

    Comments and fixes to generate_checksums.py

--- utils/generate_checksums.py
+++ utils/generate_checksums.py
@@ -1,20 +1,33 @@
+'''
+Generate sha256 checksums for pre-processed hdf5 files 
+'''
+
 import os
 import hashlib
 import pandas as pd
-import argparse
+import argparse as ap
 
 def compute_checksum(file_path):
-    """Compute the SHA256 checksum of a file."""
+    '''
+    Compute the SHA256 checksum of a file.
+    '''
     sha256_hash = hashlib.sha256()
     with open(file_path, "rb") as f:
         for byte_block in iter(lambda: f.read(4096), b""):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
 
-def generate_checksums(directory, output_csv):
-    """Generate checksums for all HDF5 files in a directory and save to a CSV."""
+def generate_checksums(input_directory, output_csv):
+    '''
+    Generate checksums for all HDF5 files in a directory and save to a CSV.
+    '''
     checksums = []
-    for root, _, files in os.walk(directory):
+
+    print('------------------------------------')
+    print(f'Generating checksums...')
+    print('------------------------------------')
+
+    for root, _, files in os.walk(input_directory):
         for file in files:
             if file.endswith('.h5'):
                 file_path = os.path.join(root, file)
@@ -23,12 +36,18 @@ def generate_checksums(directory, output_csv):
 
     df = pd.DataFrame(checksums)
     df.to_csv(output_csv, index=False)
-    print(f"Checksums saved to {output_csv}")
+
+    print(df)
+    print('------------------------------------')
+    print(f'Checksums saved to {output_csv}')
+    print('------------------------------------')
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate checksums for HDF5 files in a directory.")
-    parser.add_argument('-d', '--directory', required=True, help='Directory containing HDF5 files')
+
+    parser = ap.ArgumentParser(description="Generate checksums for HDF5 files in a directory.")
+    parser.add_argument('-i', '--input_directory', required=True, help='Directory containing HDF5 files')
     parser.add_argument('-o', '--output_csv', required=True, help='Output CSV file to save checksums')
     args = parser.parse_args()
 
-    generate_checksums(args.directory, args.output_csv)
+    generate_checksums(args.input_directory, args.output_csv)
